ae124e4dcd5c9f92c672089158edb3303e61784c
Salsabeel Tantoush <salsabeelj66@gmail.com>
2024-05-27 23:22:37 +0300

    updated membershipRoutes to check if member is in ANY team

--- back/routes/membershipRoutes.js
+++ back/routes/membershipRoutes.js
@@ -4,32 +4,24 @@ const router = express.Router();
 const admin = require('firebase-admin');
 const db = admin.firestore();
 
-// Endpoint to check if a user is a member of a team
-router.get('/checkMembership/:userId/:teamId', async (req, res) => {
-  try {
-    const { userId, teamId } = req.params;
-
-    // Check if the team exists
-    const teamDoc = await db.collection('teams').doc(teamId).get();
-    if (!teamDoc.exists) {
-      return res.status(404).send('Team not found');
+// Endpoint to check if a user is a member of any team
+router.get('/checkMembership/:userId', async (req, res) => {
+    try {
+      const { userId } = req.params;
+  
+      // Query the teams collection to check if any team includes the userId in its members array
+      const teamsSnapshot = await db.collection('teams').where('members', 'array-contains', userId).get();
+  
+      if (teamsSnapshot.empty) {
+        res.status(200).send({ isMember: false });
+      } else {
+        res.status(200).send({ isMember: true });
+      }
+    } catch (error) {
+      console.error('Error checking team membership:', error);
+      res.status(500).send('An error occurred while checking team membership');
     }
-
-    // Get the team's members
-    const teamData = teamDoc.data();
-    const members = teamData.members || [];
-
-    // Check if the user is a member of the team
-    if (members.includes(userId)) {
-      res.status(200).send({ isMember: true });
-    } else {
-      res.status(200).send({ isMember: false });
-    }
-  } catch (error) {
-    console.error('Error checking team membership:', error);
-    res.status(500).send('An error occurred while checking team membership');
-  }
-});
+  });
 
 // Export the router
 module.exports = router;
