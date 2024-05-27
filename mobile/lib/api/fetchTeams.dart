e7810c5eede5f06444965144f23454fe857a5579
RubenH11 <r.c.huygens@student.vu.nl>
2024-05-27 21:56:43 +0300

    Flutter app complete

---
+++ mobile/lib/api/fetchTeams.dart
@@ -0,0 +1,17 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:mobile/types/Participant.dart'; // For converting JSON data
+
+Future<List<Team>> fetchTeams() async {
+  print("fetching teams");
+  final response = await http.get(Uri.parse('https://hackathon-reg-malayasia.ew.r.appspot.com/api/teams'));
+
+  if (response.statusCode == 200) {
+    // If the server returns a 200 OK response, parse the JSON
+    List<dynamic> data = jsonDecode(response.body);
+    return data.map((participant) => Team.fromJson(participant)).toList();
+  } else {
+    throw Exception('Failed to load teams');
+  }
+}
--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -3,54 +3,50 @@ import 'pages/TeamsPage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
+  ThemeMode _themeMode = ThemeMode.dark;
+
+  void _toggleTheme(bool isDark) {
+    setState(() {
+      _themeMode = isDark ? ThemeMode.dark : ThemeMode.light;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Theme Toggle Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
       ),
-      home: const HomePage(title: 'Flutter Demo Home Page'),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.amber,
+      ),
+      themeMode: _themeMode,
+      home: HomePage(
+        isDarkMode: _themeMode == ThemeMode.dark,
+        toggleTheme: _toggleTheme,
+      ),
     );
   }
 }
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key, required this.title});
+  final bool isDarkMode;
+  final ValueChanged<bool> toggleTheme;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  HomePage({required this.isDarkMode, required this.toggleTheme});
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -75,8 +71,18 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Green Code Hackathon'),
+        actions: [
+          Switch(
+            value: widget.isDarkMode,
+            onChanged: widget.toggleTheme,
+            activeColor: Colors.white,
+          ),
+        ],
       ),
-      body: Padding(padding: const EdgeInsets.all(8.0), child: _pages[_currentIndex]),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: _pages[_currentIndex],
+      ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: _onTap,
